2fce5e7c3173db3aaa808ae9633f7e0277e15ec8
Sammsai <i@zcee.cc>
2026-09-10 07:24:10 +0800

    Update

--- modules/web/vite.config.ts
+++ modules/web/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
@@ -8,7 +8,7 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }): UserConfig => {
   return {
     plugins: [
       vue(),
@@ -52,7 +52,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     esbuild: {
-      drop: mode === "development" ? undefined : ["console", "debugger"],
+      drop: mode === "development" ? undefined : (["console", "debugger"] as ("console" | "debugger")[]),
     },
     build: {
       rollupOptions: {
