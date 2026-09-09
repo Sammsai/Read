45c83b24861e4aedb272ff641bc11580a65dab01
Sammsai <i@zcee.cc>
2026-09-10 07:20:57 +0800

    Update

--- modules/web/vite.config.ts
+++ modules/web/vite.config.ts
@@ -65,12 +65,5 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler', // or 'modern'
-        },
-      },
-    },
-  }
+  };
 });
